f49ea0a4e53bed6028d66c358d3038188366e86c
Humberto Pinheiro <humbhenri@gmail.com>
2017-08-17 21:09:12 -0300

    msgs de erro

--- src/main/frontend/src/app/aluno-info/aluno-info.component.ts
+++ src/main/frontend/src/app/aluno-info/aluno-info.component.ts
@@ -3,6 +3,7 @@ import {AlunoService} from '../aluno.service';
 import {Aluno} from '../aluno';
 import {Disciplina} from '../disciplina';
 import {Observable} from 'rxjs/Observable';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-aluno-info',
@@ -15,11 +16,22 @@ export class AlunoInfoComponent implements OnInit {
 
   aluno: Observable<string>;
 
-  constructor(private alunoService: AlunoService) {}
+  constructor(private alunoService: AlunoService, private flash: FlashMessagesService) {}
 
   ngOnInit() {
-    this.aluno = this.alunoService.alunoInfo().map(aluno => aluno.nome);
-    this.disciplinasFeitas = this.alunoService.alunoInfo().map(aluno => aluno.disciplinasFeitas);
+    this.aluno = this.alunoService.alunoInfo().catch((erro) => {
+      console.log(erro);
+      this.flash.show('Não foi possível buscar as informações do aluno', {cssClass: 'alert-danger', timeout: 5000});
+      return Observable.of(new Aluno(null, null, null, null));
+    }).map(aluno => aluno.nome);
+
+    this.disciplinasFeitas = this.alunoService.alunoInfo()
+    .catch(erro => {
+      console.log(erro);
+      this.flash.show('Não foi possível buscar as disciplinas do aluno', {cssClass: 'alert-danger', timeout: 5000});
+      return Observable.of(new Aluno(null, null, null, null));
+    })
+    .map(aluno => aluno.disciplinasFeitas);
   }
 
 }
